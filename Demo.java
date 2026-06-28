71a50bb48da95c00570cc0ccd388411893506000
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:50:31 +0530

    Set JFrame location to center of the screen

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ class Demo{
 	public static void main(String args[]){
 		JFrame f1=new JFrame("Calculator");	 //String->Title
 		f1.setSize(300,300);
-		//f1.setTitle("Calculator");
+		f1.setLocationRelativeTo(null);
 		f1.setVisible(true);
 	}
 }
