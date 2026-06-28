7696abd9350d589074e7549a82111becaff3a122
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:49:53 +0530

    Modify JFrame initialization to set title
    Updated JFrame instantiation to include title and commented out the setTitle method.

--- Demo.java
+++ Demo.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 class Demo{
 	public static void main(String args[]){
-		JFrame f1=new JFrame();	
+		JFrame f1=new JFrame("Calculator");	 //String->Title
 		f1.setSize(300,300);
-		f1.setTitle("Calculator");
+		//f1.setTitle("Calculator");
 		f1.setVisible(true);
 	}
 }
