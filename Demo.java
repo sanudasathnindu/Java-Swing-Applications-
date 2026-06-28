99f879a4b78f3d629a58f09f110541421df66048
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:50:50 +0530

    Comment out JFrame default close operation
    Commented out the default close operation for the JFrame.

--- Demo.java
+++ Demo.java
@@ -4,6 +4,9 @@ class Demo{
 		JFrame f1=new JFrame("Calculator");	 //String->Title
 		f1.setSize(300,300);
 		f1.setLocationRelativeTo(null);
+		//f1.setDefaultCloseOperation();
+		f1.setVisible(true);
+		try{Thread.sleep(3000);}catch(Exception ex){}
 		f1.setVisible(true);
 	}
 }
