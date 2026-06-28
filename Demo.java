12675382ab8a17194ff28e8c5c8f0fe4b979ab14
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:57:53 +0530

    Refactor button layout in Demo.java
    Replaced button layout with a new panel layout for better organization.

--- Demo.java
+++ Demo.java
@@ -8,14 +8,40 @@ class Demo{
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		f1.setLayout(new GridLayout(4,4,3,3));
-		JButton[] btArray=new JButton[16];
-		String[] buttonText={"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
-		for (int i = 0; i < 16; i++){
-			btArray[i]=new JButton(buttonText[i]);
-			btArray[i].setFont(new Font("",1,25));
-			f1.add(buttonText[i],btArray[i]);
-		}
+		JButton btNorth=new JButton("North"); //setText(String)
+		JButton btSouth=new JButton("South");
+		JButton btEast=new JButton("East");
+		JButton btWest=new JButton("West");
+		
+		JButton bt1=new JButton("1");
+		JButton bt2=new JButton("2");
+		JButton bt3=new JButton("3");
+		JButton bt4=new JButton("4");
+		
+		bt1.setFont(new Font("",1,30));
+		bt2.setFont(new Font("",1,30));
+		bt3.setFont(new Font("",1,30));
+		bt4.setFont(new Font("",1,30));
+		
+		JPanel centerPanel=new JPanel();
+		centerPanel.setLayout(new GridLayout(2,2));
+		centerPanel.add(bt1);
+		centerPanel.add(bt2);
+		centerPanel.add(bt3);
+		centerPanel.add(bt4);
+		
+		btNorth.setFont(new Font("",1,30));
+		btSouth.setFont(new Font("",1,30));
+		btEast.setFont(new Font("",1,30));
+		btWest.setFont(new Font("",1,30));
+				
+		f1.add("South",btSouth);
+		f1.add("North",btNorth);
+		f1.add("East",btEast);
+		f1.add("West",btWest);
+		f1.add(centerPanel);
+		f1.pack();
 		f1.setVisible(true);
 	}
 }
+
