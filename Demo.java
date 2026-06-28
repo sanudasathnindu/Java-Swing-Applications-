18f428e7d5cc18d0da094726042bb9a464386408
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:51:09 +0530

    Set default close operation for JFrame

--- Demo.java
+++ Demo.java
@@ -4,9 +4,13 @@ class Demo{
 		JFrame f1=new JFrame("Calculator");	 //String->Title
 		f1.setSize(300,300);
 		f1.setLocationRelativeTo(null);
-		//f1.setDefaultCloseOperation();
+		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f1.setVisible(true);
 		try{Thread.sleep(3000);}catch(Exception ex){}
 		f1.setVisible(true);
 	}
 }
+//JFrame.DO_NOTHTNG_ON_CLOSE (Static Variables)->0
+//JFrame.HIDE_ON_CLOSE (Static Variables) ->1
+//JFrame.DISPOSE_ON_CLOSE (Static Variables)->2
+//JFrame.EXIT_ON_CLOSE (Static Variables) ->3
