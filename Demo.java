1704e3e8ba2f57aa8d1e7cc95a4d329fbe02ba81
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:52:07 +0530

    Refactor JFrame setup for Calculator application

--- Demo.java
+++ Demo.java
@@ -1,16 +1,20 @@
+//Adding controllers  (JButton, JTextField, JLabel….)
+//============================================
+//JButton
+//======
 import javax.swing.*;
+import java.awt.*;
 class Demo{
 	public static void main(String args[]){
-		JFrame f1=new JFrame("Calculator1");	
-		JFrame f2=new JFrame("Calculator2");	
+		JFrame f1=new JFrame("Calculator");	
 		f1.setSize(300,300);
-		f2.setSize(300,300);
 		f1.setLocationRelativeTo(null);
-		f2.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		f2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		JButton b1=new JButton();
+		b1.setText("Hello");
+		b1.setFont(new Font("",1,30));
+		f1.add(b1);
 		f1.setVisible(true);
-		f2.setVisible(true);
 	}
 }
-
