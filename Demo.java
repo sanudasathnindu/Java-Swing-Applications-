26fe69f8782ba90b61e75c1848018355c0c53694
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:57:32 +0530

    Refactor Demo class for calculator GUI
    Updated the JFrame title and modified the layout and buttons for a calculator interface.

--- Demo.java
+++ Demo.java
@@ -1,18 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+
 class Demo{
 	public static void main(String args[]){
-		JFrame f1=new JFrame("BorderLayout");	
+		JFrame f1=new JFrame("Calculator");	
 		f1.setSize(300,300);
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		f1.setLayout(new GridLayout(3,2));
-		JButton[] btArray=new JButton[6];
-		String[] buttonText={"Add","Delete","Update","Read","Cancel","Exit"}; 
-		for (int i = 0; i < 6; i++){
+		f1.setLayout(new GridLayout(4,4,3,3));
+		JButton[] btArray=new JButton[16];
+		String[] buttonText={"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
+		for (int i = 0; i < 16; i++){
 			btArray[i]=new JButton(buttonText[i]);
-			btArray[i].setFont(new Font("",1,30));
+			btArray[i].setFont(new Font("",1,25));
 			f1.add(buttonText[i],btArray[i]);
 		}
 		f1.setVisible(true);
