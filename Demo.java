3792a66ea06a72f871a507f7fc08db1f2631090d
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 12:00:23 +0530

    Refactor main to use Calculator class and add button
    Replaced JFrame with Calculator class for main window and added a button to the Calculator.

--- Demo.java
+++ Demo.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 class Calculator extends JFrame{
 	Calculator(String title){
 		
@@ -6,19 +7,14 @@ class Calculator extends JFrame{
 } 
 class Demo{
 	public static void main(String args[]){
-		JFrame f1=new JFrame("JFrame");
-		f1.setSize(300,300);
-		f1.setLocationRelativeTo(null);
-		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f1.setVisible(true);
-		
 		Calculator c1=new Calculator("Calculator");
 		c1.setSize(300,300);
 		c1.setLocationRelativeTo(null);
 		c1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		c1.setVisible(true);
 		
+		JButton btNorth=new JButton("North");
+		btNorth.setFont(new Font("",1,25));
+		c1.add("North",btNorth);
+		c1.setVisible(true);
 	}
 }
-//Calculator ->Subclass
-//JFrame -->Super class
