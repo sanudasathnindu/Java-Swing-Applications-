de3706f0bad8ba678f92d8815751fba46e3de471
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:57:10 +0530

    Refactor JFrame layout to use GridLayout with buttons
    Removed FlowLayout JFrame and replaced it with a GridLayout in the main JFrame. Added buttons with action labels to the layout.

--- Demo.java
+++ Demo.java
@@ -1,32 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
-
 class Demo{
 	public static void main(String args[]){
 		JFrame f1=new JFrame("BorderLayout");	
-		JFrame f2=new JFrame("FlowLayout");	
 		f1.setSize(300,300);
-		f2.setSize(300,300);
 		f1.setLocationRelativeTo(null);
-		f2.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		f2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		f1.setLayout(new BorderLayout());//->Default BorderLayout
-		f2.setLayout(new FlowLayout(FlowLayout.CENTER));
-		
-		JTextField txtBorderLayout=new JTextField(5);
-		JTextField txtFlowLayout=new JTextField(5);
-		
-		txtBorderLayout.setFont(new Font("",1,30));
-		txtFlowLayout.setFont(new Font("",1,30));
-		
-		f1.add("North",txtBorderLayout);
-		f1.add("South",new JButton("Test"));
-		
-		f2.add(txtFlowLayout);
-		
+		f1.setLayout(new GridLayout(3,2));
+		JButton[] btArray=new JButton[6];
+		String[] buttonText={"Add","Delete","Update","Read","Cancel","Exit"}; 
+		for (int i = 0; i < 6; i++){
+			btArray[i]=new JButton(buttonText[i]);
+			btArray[i].setFont(new Font("",1,30));
+			f1.add(buttonText[i],btArray[i]);
+		}
 		f1.setVisible(true);
-		f2.setVisible(true);
 	}
 }
