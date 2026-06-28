d22edb8eacdf430ba839fc87c6e7db478d7c9d2c
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:52:48 +0530

    Add multiple buttons to Calculator JFrame
    Replaced single button with multiple buttons in JFrame.

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+
 class Demo{
 	public static void main(String args[]){
 		JFrame f1=new JFrame("Calculator");	
@@ -7,11 +8,29 @@ class Demo{
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		JButton b1=new JButton();
-		b1.setText("Hello");
-		b1.setFont(new Font("",1,30));
-		f1.add("South",b1);
+		JButton btNorth=new JButton("North"); //setText(String)
+		JButton btSouth=new JButton("South");
+		JButton btEast=new JButton("East");
+		JButton btWest=new JButton("West");
+		JButton btCenter=new JButton("Center");
+		btNorth.setFont(new Font("",1,30));
+		btSouth.setFont(new Font("",1,30));
+		btEast.setFont(new Font("",1,30));
+		btWest.setFont(new Font("",1,30));
+		btCenter.setFont(new Font("",1,30));
 		
+		f1.add("South",btSouth);
+		f1.add("North",btNorth);
+		f1.add("East",btEast);
+		f1.add("West",btWest);
+		f1.add("Center",btCenter);
+		f1.pack();
 		f1.setVisible(true);
 	}
 }
+
+
+
+
+
+
