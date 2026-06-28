e024fd640634ed17a9b74bb03efe5fe9afdaf806
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:51:28 +0530

    Modify JFrame titles and close operations

--- Demo.java
+++ Demo.java
@@ -1,16 +1,16 @@
 import javax.swing.*;
 class Demo{
 	public static void main(String args[]){
-		JFrame f1=new JFrame("Calculator");	 //String->Title
+		JFrame f1=new JFrame("Calculator1");	
+		JFrame f2=new JFrame("Calculator2");	
 		f1.setSize(300,300);
+		f2.setSize(300,300);
 		f1.setLocationRelativeTo(null);
-		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f2.setLocationRelativeTo(null);
+		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		f2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		f1.setVisible(true);
-		try{Thread.sleep(3000);}catch(Exception ex){}
-		f1.setVisible(true);
+		f2.setVisible(true);
 	}
 }
-//JFrame.DO_NOTHTNG_ON_CLOSE (Static Variables)->0
-//JFrame.HIDE_ON_CLOSE (Static Variables) ->1
-//JFrame.DISPOSE_ON_CLOSE (Static Variables)->2
-//JFrame.EXIT_ON_CLOSE (Static Variables) ->3
+
