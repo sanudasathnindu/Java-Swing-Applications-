ed50166c474fde5368b16b0b6c364aa576bd579c
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:58:53 +0530

    Refactor Demo class and update JFrame settings
    Updated JFrame title and modified close operation. Removed calculator UI components and added a new Calculator instance.

--- Demo.java
+++ Demo.java
@@ -1,26 +1,16 @@
 import javax.swing.*;
-import java.awt.*;
 class Demo{
 	public static void main(String args[]){
-		JFrame f1=new JFrame("Calculator");	
+		JFrame f1=new JFrame("JFrame");
 		f1.setSize(300,300);
 		f1.setLocationRelativeTo(null);
-		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
-		JTextField txtDisplay=new JTextField();
-		txtDisplay.setFont(new Font("",1,25));	
-		f1.add("North",txtDisplay);
-		
-		JPanel buttonPanel=new JPanel(); 
-		buttonPanel.setLayout(new GridLayout(4,4,3,3));
-		JButton[] btArray=new JButton[16];
-		String[] buttonText={"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
-		for (int i = 0; i < 16; i++){
-			btArray[i]=new JButton(buttonText[i]);
-			btArray[i].setFont(new Font("",1,25));
-			buttonPanel.add(buttonText[i],btArray[i]);
-		}
-		f1.add("Center",buttonPanel);
+		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f1.setVisible(true);
+		
+		Calculator c1=new Calculator("Calculator");
+		c1.setSize(300,300);
+		c1.setLocationRelativeTo(null);
+		c1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		c1.setVisible(true);
 	}
 }
