c4d6aebf7ac5c94cd7e7fbce388dcb8841609990
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:56:24 +0530

    Update JFrame layout to FlowLayout.RIGHT
    Changed layout alignment of JFrame to FlowLayout.RIGHT.

--- Demo.java
+++ Demo.java
@@ -1,13 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+
 class Demo{
 	public static void main(String args[]){
 		JFrame f1=new JFrame("Calculator");	
 		f1.setSize(300,300);
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		f1.setLayout(new FlowLayout());
 		
+		f1.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		//0->FlowLayout.CENTER
+		//1->FlowLayout.RIGHT
+		//2->FlowLayout.LEFT
 		JButton btAdd=new JButton("Add "); //setText(String)
 		JButton btDelete=new JButton("Delete");
 		JButton btUpdate=new JButton("Update");
@@ -16,6 +20,7 @@ class Demo{
 		btDelete.setFont(new Font("",1,30));
 		btUpdate.setFont(new Font("",1,30));
 		btView.setFont(new Font("",1,30));
+		
 		f1.add(btAdd);
 		f1.add(btUpdate);
 		f1.add(btDelete);
