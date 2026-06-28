410f7288e84ffd80830adc611cd169861df252b3
Sanuda Sathnindu <sanudasathnindu5@gmail.com>
2026-06-28 11:53:38 +0530

    Refactor button creation using an array

--- Demo.java
+++ Demo.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-
 class Demo{
 	public static void main(String args[]){
 		JFrame f1=new JFrame("Calculator");	
@@ -8,29 +7,15 @@ class Demo{
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		JButton btNorth=new JButton("North"); //setText(String)
-		JButton btSouth=new JButton("South");
-		JButton btEast=new JButton("East");
-		JButton btWest=new JButton("West");
-		JButton btCenter=new JButton("Center");
-		btNorth.setFont(new Font("",1,30));
-		btSouth.setFont(new Font("",1,30));
-		btEast.setFont(new Font("",1,30));
-		btWest.setFont(new Font("",1,30));
-		btCenter.setFont(new Font("",1,30));
-		
-		f1.add("South",btSouth);
-		f1.add("North",btNorth);
-		f1.add("East",btEast);
-		f1.add("West",btWest);
-		f1.add("Center",btCenter);
+		JButton[] btArray=new JButton[5];
+		String[] buttonText={"North","South","East","West","Center"}; 
+		for (int i = 0; i < 5; i++){
+			btArray[i]=new JButton(buttonText[i]);
+			btArray[i].setSize(300,400);
+			btArray[i].setFont(new Font("",1,30));
+			f1.add(buttonText[i],btArray[i]);
+		}
 		f1.pack();
 		f1.setVisible(true);
 	}
 }
-
-
-
-
-
-
