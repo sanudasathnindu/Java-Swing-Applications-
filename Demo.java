12bf7b5d8bc3543c271040d6576f02a56bf631fc
sanudasathnindu <sanudasathnindu5@gmail.com>
2026-07-01 16:51:37 +0530

    commi26

--- Demo.java
+++ Demo.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 class AddStudentForm  extends JFrame{
 	private JTextField txtDisplay;
-	
+	private JButton btCancel;
+	private JButton btAdd;
 	AddStudentForm(){
 		setSize(400,300);
 		setLocationRelativeTo(null);
@@ -12,6 +13,16 @@ class AddStudentForm  extends JFrame{
 		titleLabel.setFont(new Font("",1,27));
 		titleLabel.setHorizontalAlignment(JLabel.CENTER);
 		add("North",titleLabel);
+		
+		JPanel southPanel=new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		btAdd=new JButton("Add Student");
+		btCancel=new JButton("Cancel");
+		btAdd.setFont(new Font("",1,20));
+		btCancel.setFont(new Font("",1,20));
+		
+		southPanel.add(btAdd);
+		southPanel.add(btCancel);
+		add("South",southPanel);
 	}
 }
 class Demo{
@@ -19,4 +30,10 @@ class Demo{
 		new AddStudentForm().setVisible(true);
 	}
 }
+
+
+
+
+
+
 
