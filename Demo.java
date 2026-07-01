d98449fa7162fe0d7cd3a144c81b8d41c7a13286
sanudasathnindu <sanudasathnindu5@example.com>
2026-07-01 16:08:46 +0530

    intial commit

--- Demo.java
+++ Demo.java
@@ -1,20 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
 class Calculator extends JFrame{
+	private JButton btNorth;
+	
 	Calculator(String title){
+		setTitle(title);
+		setSize(300,300);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		btNorth=new JButton("North");
+		btNorth.setFont(new Font("",1,25));
+		add("North",btNorth);
 	}
 } 
 class Demo{
 	public static void main(String args[]){
 		Calculator c1=new Calculator("Calculator");
-		c1.setSize(300,300);
-		c1.setLocationRelativeTo(null);
-		c1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JButton btNorth=new JButton("North");
-		btNorth.setFont(new Font("",1,25));
-		c1.add("North",btNorth);
 		c1.setVisible(true);
 	}
 }
